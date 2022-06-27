406f39fe9445c3c6adb70e3824ee76f3697e2c63
noceive <nocivescience@gmail.com>
2022-06-27 01:27:13 -0400

    terminado

--- electrones.py
+++ electrones.py
@@ -1,24 +1,37 @@
 from manim import *
+import itertools as it
 class Vibrating(VGroup):
     CONFIG={
         'amplitude':0.5,
         'jingle_per_second':1,
+        'colors':[RED,YELLOW,GREEN,BLUE,PURPLE,ORANGE]
     }
     def __init__(self,group,**kwargs):
         VGroup.__init__(self,**kwargs)
+        colors=it.cycle(self.CONFIG['colors'])
         for submob in group.submobjects:
             submob.jingling_direction=rotate_vector(RIGHT,np.random.random()*TAU)
             submob.jingling_amplitude=self.CONFIG['amplitude']*np.random.random()
             submob.jingling_phase=np.random.random()*TAU
+            submob.set_color(next(colors))
             self.add(submob)
         self.add_updater(lambda m,dt:m.update(dt))
     def update(self,dt):
         for submob in self.submobjects:
             submob.jingling_phase+=np.random.random()*TAU
             submob.shift(submob.jingling_amplitude*np.sin(submob.jingling_phase))
+            submob.rotate(submob.jingling_phase)
 class Proving(Scene):
     def construct(self):
         elementos=Vibrating(VGroup(*[Square() for i in range(10)]))
         elementos.arrange(RIGHT,buff=0.5)
-        self.add(elementos)
-        self.wait(2)
+        elementos_con_numeros=self.number_of_elements(elementos)
+        self.add(elementos_con_numeros)
+        self.wait(3)
+    def number_of_elements(self,mobs):
+        for i in range(len(mobs)):
+            texto=Integer(i)
+            texto.set_color(WHITE)
+            texto.move_to(mobs[i].get_center())
+            mobs[i].add(texto)
+        return mobs
